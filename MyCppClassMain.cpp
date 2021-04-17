ed4892ccf49eb70406c599516cfe1ca29397c5c5
Kouichi Umezawa <kouichiume@gmail.com>
2021-04-18 01:25:56 +0900

    sample c++

---
+++ MyCppClassMain.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kouichi on 21/04/18.
+//
+
+#include "MyCppClassMain.h"
---
+++ MyCppClassMain.h
@@ -0,0 +1,16 @@
+//
+// Created by kouichi on 21/04/18.
+//
+
+#ifndef MY_APPLICATION_MYCPPCLASSMAIN_H
+#define MY_APPLICATION_MYCPPCLASSMAIN_H
+
+
+
+class MyCppClassMain {
+
+};
+
+
+
+#endif //MY_APPLICATION_MYCPPCLASSMAIN_H
