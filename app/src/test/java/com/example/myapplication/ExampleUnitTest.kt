98f0d2da780e845be966df0eab65dd79f1103da6
kouichiUme <kouichiume@gmail.com>
2021-04-19 23:52:07 +0900

    debug

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -11,7 +11,10 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
+    function addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+
 }
