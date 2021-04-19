502ed267b976ff681ce97b80311cafb91c45664e
kouichiUme <kouichiume@gmail.com>
2021-04-19 23:58:34 +0900

    debug

---
+++ app/src/test/java/com/example/myapplication/ExampleTestUnit.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTestUnit {
+
+
+    /**
+     * Example local unit test, which will execute on the development machine (host).
+     *
+     * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+     */
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
